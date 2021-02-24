5f9bfca8c7cee57b68db7f357c2ce2c8f970c295
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-02-24 18:08:07 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Options from './components/Options';
+import Output from './components/Output';
 
 function App() {
+  const [paragraphs, setParagraphs] = useState([]);
+  const [tag, setTag] = useState('p');
+  const [inputValue, setInputValue] = useState(1);
+  const [includeHtml, setIncludeHtml] = useState("Yes");
+
+  useEffect(() => {
+    const url = `https://baconipsum.com/api/?type=all-meat&paras=${inputValue}&start-with-lorem=1`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setParagraphs(data));
+  }, [inputValue])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="title">
+          <h1>Dummy Text Generator</h1>
+        </div>
+        <Options
+          paragraphs={paragraphs}
+          includeHtml={includeHtml}
+          setIncludeHtml={setIncludeHtml}
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          tag={tag}
+          setTag={setTag}
+        />
+        <Output
+          paragraphs={paragraphs}
+          tag={tag}
+          includeHtml={includeHtml}
+        />
+      </div>
     </div>
   );
 }
